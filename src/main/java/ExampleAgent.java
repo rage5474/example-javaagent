e7c9830843766a673d996157d63cf3a67b74dc99
rage5474 <raphael.geissler@gmail.com>
2017-11-11 20:35:00 +0100

    Initial commit

---
+++ src/main/java/ExampleAgent.java
@@ -0,0 +1,9 @@
+import java.lang.instrument.Instrumentation;
+
+public class ExampleAgent {
+	
+	public static void premain(String args, Instrumentation inst) {
+		System.out.println("Starting instrumentation...");
+		
+	}
+}
